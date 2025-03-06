c7fab6e807b4086dd6641477cce382cf5bfe91d4
piyush choudhary <piyushchoudhary.068@gmail.com>
2025-03-06 10:03:55 +0530

    change

--- layers-frontend/src/components/Hero.tsx
+++ layers-frontend/src/components/Hero.tsx
@@ -64,11 +64,11 @@ export function Hero(){
         Impactful Design, Created Effortlessly
         </div>
         <div className="text-center mt-4 text-white/60   pt-2">
-        Design tool should not slow y ou down. Layers combine powerfull features with an intuitive interface that keeps you in your creative flow.
+        Design tool should not slow you down. Layers combine powerfull features with an intuitive interface that keeps you in your creative flow.
         </div>
         <div className="flex border-1 mt-10  rounded-3xl p-1 border-white/70  ">
 <input type="text" className="flex-1  lg:w-72 md:w-64 w-56  rounded-3xl p-2" placeholder="Enter your email"></input>
-<button className="bg-[#A3E635] text-black text-sm md:text-lg  rounded-3xl h-9 px-5 ">Sign Up</button>
+<button className="bg-[#A3E635] text-black text-xs md:text-lg  rounded-3xl h-9 px-5 ">Sign Up</button>
 
         </div>
        </div>
